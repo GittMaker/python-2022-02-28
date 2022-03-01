777ef08f40d920d7064a866a9dd26da8ad3b5630
GittMaker <tothzoltan.matek@gmail.com>
2022-03-01 10:48:40 +0100

    refactoring welcome_page_test.py

--- welcome_page_test.py
+++ welcome_page_test.py
@@ -1,23 +1,23 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-def test_welcome_page_header():
-    # Given
+@pytest.fixture
+def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    # Then
     driver.get("https://gittmaker.github.io/python-2022-02-28/index.html")
+    return driver
+
+
+def test_welcome_page_header(driver):
     header = driver.find_element(By.TAG_NAME, "h1").text
     # Then
     assert header == "Welcome"
 
 
-def test_welcome_page_paragraph():
-    # Given
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    # Then
-    driver.get("https://gittmaker.github.io/python-2022-02-28/index.html")
+def test_welcome_page_paragraph(driver):
     header = driver.find_element(By.TAG_NAME, "p").text
     # Then
-    assert header == "Welcome to the python trainig"
+    assert header == "Welcome to the python training!"
