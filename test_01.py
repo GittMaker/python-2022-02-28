ab3f664b0221e24d7677dc8d2a63cc107ef8c989
GittMaker <tothzoltan.matek@gmail.com>
2022-03-01 09:28:29 +0100

    Refacoring test_01.py

--- test_01.py
+++ test_01.py
@@ -1,5 +1,6 @@
-def is_bigger_than_15(number:int) -> bool:
+def is_larger_than_15(number:int) -> bool:
     return number > 15
 
-print(is_bigger_than_15(30))
-print(is_bigger_than_15(2))
+
+print(is_larger_than_15(30))
+print(is_larger_than_15(2))
