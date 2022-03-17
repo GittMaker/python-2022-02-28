acea3153bf2f9c823aad8dabd9d327fc878dc5c5
GittMaker <tothzoltan.matek@gmail.com>
2022-03-17 10:11:32 +0100

    String methods

---
+++ data_structures.py
@@ -0,0 +1,21 @@
+#Összetett adattípusok
+
+#str, list -> rendezett listák
+fruit = "cseresznye"
+print(fruit[:5])
+
+print(fruit.upper())
+print(fruit)
+
+print('eresz' in fruit)
+
+print("    text   ".strip())
+
+print(fruit.index('e'))
+
+print(','.join(["a","b","c"]))
+
+paragraph = "alma korte barack meggy"
+fruits = paragraph.split()
+print(fruits)
+
