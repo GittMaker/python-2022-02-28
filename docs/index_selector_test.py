e48c1423bf218213342ce476facff9a49bc6ee08
GittMaker <tothzoltan.matek@gmail.com>
2022-03-18 08:51:05 +0100

    Morning test

--- docs/index_selector_test.py
+++ docs/index_selector_test.py
@@ -2,7 +2,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 
-
+# 
 def test_relative_selector(driver: WebDriver):
     driver.get("http://127.0.0.1:5500/docs/index.html")
     form = driver.find_element(By.ID, "form0317")
